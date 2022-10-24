f259dde0f16bd50059d38898b08a8234714235a5
Rikunz <88525167+Rikunz@users.noreply.github.com>
2022-10-24 16:15:09 +0700

    Update dataBarang.js

--- js/dataBarang.js
+++ js/dataBarang.js
@@ -1,2 +1,6 @@
 let timenow = new Date();
-document.getElementById("lastupdate").innerHTML = "Last update:" + timenow;
+var minutes = timenow.getMinutes();
+if (minutes < 10){
+    minutes = "0" + minutes;
+}
+document.getElementById("lastupdate").innerHTML = "Last update:" + timenow.getDate() + "/" + timenow.getMonth() + "/" + timenow.getFullYear() + ". " + timenow.getHours() + ":" + minutes;
