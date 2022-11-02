074e4cd39c6ee9b8506a0c5cf1d9db3f4860661a
Rikunz <88525167+Rikunz@users.noreply.github.com>
2022-11-02 16:48:00 +0700

    Delete dataBarang.js

--- js/dataBarang.js
+++
@@ -1,6 +0,0 @@
-let timenow = new Date();
-var minutes = timenow.getMinutes();
-if (minutes < 10){
-    minutes = "0" + minutes;
-}
-document.getElementById("lastupdate").innerHTML = "Last update:" + timenow.getDate() + "/" + timenow.getMonth() + "/" + timenow.getFullYear() + ". " + timenow.getHours() + ":" + minutes;
